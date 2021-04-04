6081d576632b1e9c4b06e53b739201e4205535bc
RishikaPal <80198528+RishikaPal@users.noreply.github.com>
2021-04-04 11:22:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,40 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var helecopterIMG,helecopterSprite,packageSprite,packageIMG;
+var packagebody,ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+function preload() {
+    helecopterIMG=loadImage("helecopter.png")
+    packageIMG=loadImage("package.png")
 }
-
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
-	Engine.run(engine);
-  
+    createCanvas(800,700);
+    rectMode(CENTER);
+    packageSprite=createSprite(width/2,80,10,10);
+    packageSprite.addImage(packageIMG)
+    packageSprite.scale=0.2
+    helecopterSprite=createSprite(width/2,200,10,10);
+    helecopterSprit.addImage(helecopterIMG)
+    helecopterSprite.scale=0.6
+    groundSprite=createSprite(width/2,200,10,10);
+    groundSprite.shapeColour=colour(225)
+    engine=Engine.create();
+    World=engine.world;
+   packagebody=Bodies.circle(width/2,200,5,{restution:04,isStatic:true});
+   world.add(world,packagebody)
+  ground=Bodies.rectangle(width/2,650,width,10,{isStatic:true});
+   world.add(world,ground);
+   Engine.run(engine);
 }
-
-
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
- 
+    rectMode(CENTER);
+    background(0);
+    packageSprite.x=packagebody.position.x
+    packageSprite.y=packagebody.position.y
+    drawSprites();
 }
+if (keyCode === DOWN_ARROW) {
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-
-    
-  }
-}
-
-
-
+Matter.Body.setStatic(packageBody,false);
+ }
