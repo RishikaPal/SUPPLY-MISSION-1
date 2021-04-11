85f2d2db1f072344b783ace5a4c6f307f383559a
RishikaPal <80198528+RishikaPal@users.noreply.github.com>
2021-04-11 11:16:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,29 +4,37 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+
 function preload() {
-    helecopterIMG=loadImage("helecopter.png")
+    helecopterIMG=loadImage("helicopter.png")
     packageIMG=loadImage("package.png")
 }
+
 function setup() {
     createCanvas(800,700);
     rectMode(CENTER);
     packageSprite=createSprite(width/2,80,10,10);
     packageSprite.addImage(packageIMG)
     packageSprite.scale=0.2
+
     helecopterSprite=createSprite(width/2,200,10,10);
-    helecopterSprit.addImage(helecopterIMG)
+    helecopterSprite.addImage(helecopterIMG)
     helecopterSprite.scale=0.6
-    groundSprite=createSprite(width/2,200,10,10);
-    groundSprite.shapeColour=colour(225)
+
+    groundSprite=createSprite(width/2,height-35,width,10);
+    groundSprite.shapeColor=color(225)
+
     engine=Engine.create();
-    World=engine.world;
+    world=engine.world;
+
    packagebody=Bodies.circle(width/2,200,5,{restution:04,isStatic:true});
-   world.add(world,packagebody)
+   World.add(world,packagebody)
+
   ground=Bodies.rectangle(width/2,650,width,10,{isStatic:true});
-   world.add(world,ground);
+   World.add(world,ground);
    Engine.run(engine);
 }
+
 function draw() {
     rectMode(CENTER);
     background(0);
@@ -34,7 +42,10 @@ function draw() {
     packageSprite.y=packagebody.position.y
     drawSprites();
 }
-if (keyCode === DOWN_ARROW) {
 
-Matter.Body.setStatic(packageBody,false);
- }
+function keyPressed() {
+    if (keyCode === DOWN_ARROW) {
+         Matter.Body.setStatic(packagebody,false);
+     }
+   }
+   
